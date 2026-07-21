0b54c27de6d4b8694899aa7ff5764b122add8afa
Thomas Barker <146092737+Thomas-BBarker@users.noreply.github.com>
2026-07-21 22:26:16 +0000

    FIXES

--- riskassessment/settings.py
+++ riskassessment/settings.py
@@ -29,15 +29,14 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "testserver",
+    ".app.github.dev",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
-    "https://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://127.0.0.1:8000",
+    "https://*.app.github.dev",
 ]
-
 
 # Application definition
 
